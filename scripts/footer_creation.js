4e0e2bfa1aba695c3360b0f3c56467d1efe7b61f
dhalian <dhalian2005gmail.com>
2025-08-29 13:05:24 +0200

    Ajout d'avertissements de maintenance pour les partenaires et rectification du numéro de version dans le pied de page

--- scripts/footer_creation.js
+++ scripts/footer_creation.js
@@ -24,7 +24,7 @@ footer.innerHTML = `
     <div class="left-footer">
         <div class="hbf-logo-container" style="padding: 0px; margin: 0px; cursor: pointer;">
             <p style="font-family: Sora; font-size: 24px; color: white;"><span style="font-weight: 300;">Homebrew</span><strong>France</strong></p>
-            <div class="hbf-version">Version : 4.0.0</div>
+            <div class="hbf-version">Version : 4.1.0</div>
         </div>
         <small style="color: rgb(210, 210, 210); font-family: 'Poppins',sans-serif;"><a class="alcool" href="https://homebrewfrance.fr/multi-checker/alcoolemie">©</a> 2021 - 2025&nbsp;<span style="color: rgb(173, 173, 173)">|</span>&nbsp;Distribué sous licence <a href="https://homebrewfrance.fr/licence/">GNU GPLv3</a></small>
     </div>
