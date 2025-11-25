b93d87c8926fdd9dade3d7b3359a7e3c1741b460
Dhalian <dhalian2005@gmail.com>
2025-11-25 15:21:49 +0100

    Retour en arrière

--- scripts/navigationBar_creation.js
+++ scripts/navigationBar_creation.js
@@ -15,44 +15,32 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
-var htmlpage = document.getElementsByTagName('html')[0];
-var websiteIcon = "https://cdn.homebrew-france.fun/generic/beta_website-icon.png";
-var websiteIconTitle = "Homebrew France";
-var optionalClass = "";
-
-if (pageID && pageID.startsWith('DOC')) {
-    websiteIcon = "https://cdn.homebrew-france.fun/generic/lumia-guides.png";
-    websiteIconTitle = "Lumia Guides - par Homebrew France";
-    optionalClass = 'class="lumiaIconNav"';
+function isValidNavigationItem(item) {
+    return typeof item === 'object' &&
+           typeof item.id === 'string' &&
+           typeof item.name === 'string' &&
+           typeof item.category === 'string' &&
+           (typeof item.url === 'string' || item.url === undefined);
 }
 
-htmlpage.insertAdjacentHTML('afterbegin', `
-<div class="navbar-container">
-  <div class="navbar">
-    <div id="navContainer">
-      <img id="navIcon" src="${websiteIcon}" ${optionalClass} style="height: 45px; width: 45px;" title="${websiteIconTitle}">
-      <div id="navbarToggler">
-        <img id="hamburgerMenu" src="https://cdn.homebrew-france.fun/generic/hamburger-icon.png" style="height: 38px; width: 38px;">
-      </div>
-    </div>
-    <span id="desktopItems">
-      <a class="nav-link" id="accueilLink" href="https://homebrewfrance.fr/"><span id="accueilLinkSpan">Accueil</span></a>
-      <a class="nav-link" id="telechargementsLink" href="https://homebrewfrance.fr/ressources/"><span id="telechargementsLinkSpan">Ressources</span></a>
-      <a class="dropdown-item" id="docsDrop"><span class="nav-item-content">Guides</span><div class="dropdown-menu animated fadeIn" id="docsDrop-menu"></div></a>
-      <a class="dropdown-item" id="multicheckerDrop"><span class="nav-item-content">Checker</span><div class="dropdown-menu animated fadeIn" id="multicheckerDrop-menu"></div></a>
-      <a class="nav-link" id="aproposLink" href="https://homebrewfrance.fr/a-propos/"><span id="aproposLinkSpan">À propos</span></a>
-    </span>
-    <span id="mobileItems">
-      <a class="nav-link" id="accueilLink" href="https://homebrewfrance.fr/"><span id="accueilLinkSpan">Accueil</span></a>
-      <a class="nav-link" id="telechargementsLink" href="https://homebrewfrance.fr/ressources/"><span id="telechargementsLinkSpan">Ressources</span></a>
-      <a class="dropdown-item" id="docsDrop"><span class="nav-item-content">Guides</span><div class="dropdown-menu animated fadeIn" id="docsDrop-menu"></div></a>
-      <a class="dropdown-item" id="multicheckerDrop"><span class="nav-item-content">Checker</span><div class="dropdown-menu animated fadeIn" id="multicheckerDrop-menu"></div></a>
-      <a class="nav-link" id="aproposLink" href="https://homebrewfrance.fr/a-propos/"><span id="aproposLinkSpan">À propos</span></a>
-    </span>
-  </div>
-</div>
-`);
+function validateNavigationItems(data) {
+    if (!Array.isArray(data)) return false;
+    return data.every(isValidNavigationItem);
+}
+
+function isValidDropdownItem(item) {
+    return typeof item === 'object' &&
+           typeof item.id === 'string' &&
+           typeof item.name === 'string' &&
+           typeof item.dropdownId === 'string' &&
+           (typeof item.url === 'string' || item.id.includes('grayed')) &&
+           (typeof item.iconLink === 'string' || item.iconLink === undefined);
+}
+
+function validateDropdownItems(data) {
+    if (!Array.isArray(data)) return false;
+    return data.every(isValidDropdownItem);
+}
 
 var htmlpage = document.getElementsByTagName('html')[0];
 const body = document.getElementsByTagName('body')[0];
@@ -65,6 +53,24 @@ if (pageID.startsWith('DOC')) {
     var optionalClass = "class=\"lumiaIconNav\"";
 }
 
+const nav = document.createElement('div');
+nav.className = 'navbar-container'
+nav.innerHTML = `
+<div class="navbar">
+    <div id="navContainer">
+        <img id="navIcon" src="${websiteIcon}" ${optionalClass} style="height: 45px; width: 45px;" title="${websiteIconTitle}">
+        <div id="navbarToggler">
+            <img id="hamburgerMenu" src="https://cdn.homebrew-france.fun/generic/hamburger-icon.png" style="height: 38px; width: 38px;">
+        </div>
+    </div>
+    <span id="desktopItems">
+    </span>
+    <span id="mobileItems">
+    </span>
+</div>
+`;
+
+htmlpage.appendChild(nav);
 
 const navIcon = document.getElementById('navIcon');
 navIcon.onclick = function() {
@@ -75,6 +81,106 @@ const mobileItems = document.getElementById('mobileItems');
 
 const desktopItems = document.getElementById('desktopItems');
 
+function createNavbarItemsDesktop(navigationItems) {
+    for (let index = 0; index < navigationItems.length; index++) {
+        const navItem = document.createElement('a');
+        navItem.className = navigationItems[index].category;
+        navItem.id = navigationItems[index].id;
+        if (navigationItems[index].url) {
+            navItem.href = navigationItems[index].url;
+        }
+        if (navItem.className === 'dropdown-item') {
+            navItem.innerHTML = `
+            <span class="nav-item-content">
+                ${navigationItems[index].name}
+            </span>
+            <div class="dropdown-menu animated fadeIn" id="${navigationItems[index].id + '-menu'}">
+            `;
+        }
+        else {
+            navItem.innerHTML = `<span id="${navigationItems[index].id + "Span"}">${navigationItems[index].name}</span>`;
+        }
+        desktopItems.appendChild(navItem);
+    }
+}
+
+function createNavbarItemsMobile(navigationItems) {
+    for (let index = 0; index < navigationItems.length; index++) {
+        const navItem = document.createElement('a');
+        navItem.className = navigationItems[index].category;
+        navItem.id = navigationItems[index].id;
+        if (navigationItems[index].url) {
+            navItem.href = navigationItems[index].url;
+        }
+        if (navItem.className === 'dropdown-item') {
+            navItem.innerHTML = `
+            <span class="nav-item-content">
+                ${navigationItems[index].name}
+            </span>
+            <div class="dropdown-menu animated fadeIn" id="${navigationItems[index].id + '-menu'}">
+            `;
+        }
+        else {
+            navItem.innerHTML = `<span id="${navigationItems[index].id + "Span"}">${navigationItems[index].name}</span>`;
+        }
+        mobileItems.appendChild(navItem);
+    }
+}
+
+function createDropdownItemsDesktop(dropdownsItems) {
+    for (let index = 0; index < dropdownsItems.length; index++) {
+        const dropItem = document.createElement('a');
+        if (dropdownsItems[index].id.includes('grayed')) {
+            dropItem.className = 'drop-item grayed';
+            dropItem.title = 'Bientôt disponible!';
+        }
+        else {
+            dropItem.className = 'drop-item';
+            dropItem.href = dropdownsItems[index].url;
+        }
+        dropItem.id = dropdownsItems[index].id;
+        if (dropdownsItems[index].iconLink) {
+            dropItem.innerHTML = `
+            <img src="${dropdownsItems[index].iconLink}" width="20px" height="20px" style="margin-right: 9px;">
+            ${dropdownsItems[index].name}
+        `;
+        } else {
+            dropItem.innerHTML = dropdownsItems[index].name;
+        }
+        const dropdownTargetId = dropdownsItems[index].dropdownId + '-menu';
+        const dropdownTarget = document.querySelector('#desktopItems div#' + dropdownTargetId);
+        if (dropdownTarget) {
+            dropdownTarget.appendChild(dropItem);
+        }
+    }
+}
+
+function createDropdownItemsMobile(dropdownsItems) {
+    for (let index = 0; index < dropdownsItems.length; index++) {
+        const dropItem = document.createElement('a');
+        dropItem.href = dropdownsItems[index].url;
+        if (dropdownsItems[index].id.includes('grayed')) {
+            dropItem.className = 'drop-item grayed';
+        }
+        else {
+            dropItem.className = 'drop-item';
+        }
+        dropItem.id = dropdownsItems[index].id;
+        if (dropdownsItems[index].iconLink) {
+            dropItem.innerHTML = `
+            <img src="${dropdownsItems[index].iconLink}" width="20px" height="20px" style="margin-right: 9px;">
+            ${dropdownsItems[index].name}
+        `;
+        } else {
+            dropItem.innerHTML = dropdownsItems[index].name;
+        }
+        const dropdownTargetId = dropdownsItems[index].dropdownId + '-menu';
+        const dropdownTarget = document.querySelector('#mobileItems div#' + dropdownTargetId);
+        if (dropdownTarget) {
+            dropdownTarget.appendChild(dropItem);
+        }
+    }
+}
 
 function updateNavbarWidth() {
     var navbarElement = document.getElementsByClassName('navbar')[0];
@@ -94,60 +200,106 @@ document.addEventListener('DOMContentLoaded', function () {
         mobileNavMenu.classList.toggle("showLinks");
     });
 
-    updateNavbarWidth();
-    window.addEventListener('resize', updateNavbarWidth);
 
-    document.querySelectorAll('.dropdown-item').forEach(item => {
-        const dropdownMenuDesktop = document.querySelector('#desktopItems div#' + item.id + '-menu');
-        if (dropdownMenuDesktop) {
-            item.addEventListener('click', function() {
-                var desktopItemsCheck = document.getElementById('desktopItems');
-                var otherDrop = desktopItemsCheck.getElementsByClassName('showDropdown')[0];
-                var allDrops = desktopItemsCheck.getElementsByClassName('showDropdown');
-                dropdownMenuDesktop.classList.toggle('showDropdown');
+    fetch('https://homebrewfrance.fr/json/navigation-items.json')
+        .then(response => response.json())
+        .then(data => {
+            const navigationItems = data.navigationItems;
+            if (validateNavigationItems(navigationItems)) {
+                createNavbarItemsDesktop(navigationItems);
+                createNavbarItemsMobile(navigationItems);
+            } else {
+                console.error('Erreur : données de navigation invalides.');
+            }
+            updateNavbarWidth();
+            window.addEventListener('resize', updateNavbarWidth);
 
-                if (otherDrop) {
-                    otherDrop.classList.remove('showDropdown');
+            document.querySelectorAll('.dropdown-item').forEach(item => {
+                const dropdownMenuDesktop = document.querySelector('#desktopItems div#' + item.id + '-menu');
+                if (dropdownMenuDesktop) {
+                    item.addEventListener('click', function() {
+                        var desktopItemsCheck = document.getElementById('desktopItems');
+                        var otherDrop = desktopItemsCheck.getElementsByClassName('showDropdown')[0];
+                        var allDrops = desktopItemsCheck.getElementsByClassName('showDropdown');
+                        dropdownMenuDesktop.classList.toggle('showDropdown');
+
+                        if (otherDrop) {
+                            otherDrop.classList.remove('showDropdown');
+                        }
+
+                        if (usrAgent.includes('Chrome')) {
+                            if (allDrops.length > 0) {
+                                var navbar = document.getElementsByClassName('navbar')[0];
+                                navbar.style.backdropFilter = 'none';
+                                navbar.style.backgroundColor = '#151518e5';
+                            }
+                            else {
+                                var navbar = document.getElementsByClassName('navbar')[0];
+                                navbar.style.backdropFilter = 'blur(10px)';
+                            }
+                        }
+                    });
                 }
 
-                if (usrAgent.includes('Chrome')) {
-                    var navbar = document.getElementsByClassName('navbar')[0];
-                    navbar.style.backdropFilter = (allDrops.length > 0) ? 'none' : 'blur(10px)';
-                    if (allDrops.length > 0) navbar.style.backgroundColor = '#151518e5';
+                const dropdownMenuMobile = document.querySelector('#mobileItems div#' + item.id + '-menu');
+                if (dropdownMenuMobile) {
+                    item.addEventListener('click', function() {
+                        var mobileItemsCheck = document.getElementById('mobileItems');
+                        var otherDrop = mobileItemsCheck.getElementsByClassName('showDropdown')[0];
+                        dropdownMenuMobile.classList.toggle('showDropdown');
+                        if (otherDrop) {
+                            otherDrop.classList.remove('showDropdown');
+                        }
+                    });
                 }
             });
-        }
+        })
+        .catch(error => console.error('Erreur:', error));
+        var navbarEl = document.getElementsByClassName('navbar')[0];
+        if (mobileNavMenu.className == '') {
 
-        const dropdownMenuMobile = document.querySelector('#mobileItems div#' + item.id + '-menu');
-        if (dropdownMenuMobile) {
-            item.addEventListener('click', function() {
-                var mobileItemsCheck = document.getElementById('mobileItems');
-                var otherDrop = mobileItemsCheck.getElementsByClassName('showDropdown')[0];
-                dropdownMenuMobile.classList.toggle('showDropdown');
-                if (otherDrop) otherDrop.classList.remove('showDropdown');
-            });
         }
-    });
-
-    var navbarEl = document.getElementsByClassName('navbar')[0];
-    if (mobileNavMenu.className != '') navbarEl.classList.toggle('navbarMobileRadius');
-
-    var navLinks = document.getElementsByClassName('nav-link');
-    var dropLinks = document.getElementsByClassName('dropdown-item');
-
-    for (var index=0; index<navLinks.length; index++) {
-        if ((navLinks[index].id == 'accueilLink' && pageID == 'HBF-Home') ||
-            (navLinks[index].id == 'telechargementsLink' && pageID == 'TELECHARGEMENTS') ||
-            (navLinks[index].id == 'recrutementsLink' && pageID == 'RECRUTEMENTS') ||
-            (navLinks[index].id == 'boutiqueLink' && pageID == 'BOUTIQUE') ||
-            (navLinks[index].id == 'aproposLink' && pageID == 'A-PROPOS')) {
-            navLinks[index].className = "nav-link active";
+        else {
+            navbarEl.classList.toggle('navbarMobileRadius');
         }
-    }
-    for (var index=0; index<dropLinks.length; index++) {
-        if ((dropLinks[index].id == 'docsDrop' && pageID.startsWith('DOC')) ||
-            (dropLinks[index].id == 'multicheckerDrop' && pageID.startsWith('MLTI'))) {
-            dropLinks[index].className = "dropdown-item active";
-        }
-    }
+        
+    fetch('https://homebrewfrance.fr/json/dropdowns-items.json')
+        .then(response => response.json())
+        .then(data => {
+            const dropdownsItems = data.dropdownsItems;
+            var dropdownItemNavbar = document.getElementsByClassName('dropdown-item')[0];
+            if (dropdownItemNavbar) {
+                if (validateDropdownItems(dropdownsItems)) {
+                    createDropdownItemsDesktop(dropdownsItems);
+                    createDropdownItemsMobile(dropdownsItems);
+                } else {
+                    console.error('Erreur : données de navigation invalides.');
+                }
+                var navLinks = document.getElementsByClassName('nav-link');
+                var dropLinks = document.getElementsByClassName('dropdown-item');
+            
+                for (var index=0; index<navLinks.length; index++) {
+                    if ((navLinks[index].id == 'accueilLink' && pageID == 'HBF-Home') || (navLinks[index].id == 'telechargementsLink' && pageID == 'TELECHARGEMENTS') || (navLinks[index].id == 'recrutementsLink' && pageID == 'RECRUTEMENTS') || (navLinks[index].id == 'boutiqueLink' && pageID == 'BOUTIQUE') || (navLinks[index].id == 'aproposLink' && pageID == 'A-PROPOS')) {
+                        navLinks[index].className = "nav-link active";
+                    }
+                    else {
+                    }
+                }
+                for (var index=0; index<dropLinks.length; index++) {
+                    if ((dropLinks[index].id == 'docsDrop' && pageID.startsWith('DOC')) || (dropLinks[index].id == 'multicheckerDrop' && pageID.startsWith('MLTI'))) {
+                        dropLinks[index].className = "dropdown-item active";
+                    }
+                    else {
+                    }
+                }
+                
+            }
+            else {
+                setTimeout(function() {
+                    createDropdownItemsDesktop(dropdownsItems);
+                    createDropdownItemsMobile(dropdownsItems);
+                }, 2000);
+            }
+        })
+        .catch(error => console.error('Erreur:', error));
 });
